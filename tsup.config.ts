b7de464cabe37bd3aca8a342608de42cfb304954
baichaohua <84051225@qq.com>
2024-12-09 14:28:24 +0800

    add auto verison

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, type Options } from 'tsup'
+import autoVersion from './plugin/autoVersion'
 
 const defaultConfig: Options = {
+  esbuildPlugins: [
+    autoVersion()
+  ],
   clean: true,
   dts: true,
   target: 'esnext',
