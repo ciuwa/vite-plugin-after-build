b316564b28f53252c60081e9131732b5dfa70a89
baichaohua <84051225@qq.com>
2024-12-09 15:56:08 +0800

    Release v0.0.9, build on 2024-12-09T07:56:08.253Z

---
+++ git.release.cjs
@@ -0,0 +1,48 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const packagePath = path.join(__dirname, './package.json');
+const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+
+pkg.version = incrementVersion(pkg.version)
+
+fs.writeFileSync(packagePath, JSON.stringify(pkg, null, 2), 'utf8');
+
+try {
+  const newVersion = 'v' + pkg.version;
+  execSync('git add .', { stdio: 'inherit' });
+
+  const commitMessage = `Release ${newVersion}, build on ${new Date().toISOString()}`;
+  execSync(`git commit -m "${commitMessage}" --no-verify`, { stdio: 'inherit' });
+
+  // 5. 创建标签  
+  execSync(`git tag -a ${newVersion} -m "Release version ${newVersion}"`, { stdio: 'inherit' });
+
+  // 6. 推送到远程仓库  
+  execSync('git push origin main', { stdio: 'inherit' });
+  // execSync(`git push origin v${newVersion}`, { stdio: 'inherit' });
+  console.log(`Version updated ${newVersion}`);
+} catch (error) {
+  console.error('Failed to commit changes or create a tag:', error.message);
+}
+
+
+function incrementVersion(currentVersion, threshold) {
+  const [major, minor, patch] = currentVersion.split('.').map(Number);
+
+  let newPatch = patch + 1;
+  if (newPatch > threshold) {
+    newPatch = 0;
+    let newMinor = minor + 1;
+    if (newMinor > threshold) { // 通常 minor 不会有上限，这里仅按您的要求处理
+      newMinor = 0;
+      const newMajor = major + 1;
+      return `${newMajor}.${newMinor}.${newPatch}`;
+    } else {
+      return `${major}.${newMinor}.${newPatch}`;
+    }
+  } else {
+    return `${major}.${minor}.${newPatch}`;
+  }
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig, type Options } from 'tsup'
-import autoVersion from './plugin/autoVersion'
 
 const defaultConfig: Options = {
-  esbuildPlugins: [
-    autoVersion()
-  ],
   clean: true,
   dts: true,
   target: 'esnext',
