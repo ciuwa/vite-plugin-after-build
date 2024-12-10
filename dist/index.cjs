b95794b03318523709acf88188effbf2056af4ea
baichaohua <84051225@qq.com>
2024-12-10 16:19:42 +0800

    Release v0.0.16, build on 2024-12-10T08:19:42.017Z

--- dist/index.cjs
+++ dist/index.cjs
@@ -35,6 +35,8 @@ __export(src_exports, {
 module.exports = __toCommonJS(src_exports);
 var import_node_path = __toESM(require("path"), 1);
 var import_node_fs = __toESM(require("fs"), 1);
+var import_basic_ftp = require("basic-ftp");
+var import_node_child_process = require("child_process");
 
 // src/utils.ts
 function incrementVersion(currentVersion, threshold) {
@@ -54,38 +56,116 @@ function incrementVersion(currentVersion, threshold) {
     return `${major}.${minor}.${newPatch}`;
   }
 }
+function log(...messages) {
+  console.log("\u3010vite-plugin-after-build\u3011" + messages.join(" "));
+}
 
 // src/index.ts
-function autoVersionPlugin(options = {}) {
-  let buildSuccessful = false;
-  let oldVersion = "";
-  const pkgPath = import_node_path.default.resolve(process.cwd(), "package.json");
-  const disabled = options.disabled ?? false;
-  if (disabled) {
-    return {
-      name: "vite-plugin-after-build"
-    };
+var import_lodash_es = require("lodash-es");
+var defaultOptions = {
+  updateVersion: {
+    enable: true,
+    threshold: 100
+  },
+  gitCommit: {
+    enable: false,
+    push: false
+  },
+  ftpUpload: {
+    enable: false,
+    host: "",
+    user: "",
+    password: "",
+    secure: false,
+    localPath: "",
+    remotePath: "",
+    verbose: false
   }
+};
+var oldVersion = "";
+var newVersion = "";
+var pkgPath = import_node_path.default.resolve(process.cwd(), "package.json");
+function autoVersionPlugin(options = {}) {
+  const __options = (0, import_lodash_es.merge)(defaultOptions, options);
+  let buildSuccessful = false;
   return {
     name: "vite-plugin-after-build",
     buildStart() {
-      const pkg = JSON.parse(import_node_fs.default.readFileSync(pkgPath, "utf-8"));
-      oldVersion = pkg.version;
-      const max = options.threshold ?? 100;
-      const newVersion = incrementVersion(pkg.version, max);
-      pkg.version = newVersion;
-      import_node_fs.default.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), "utf-8");
-      console.log(`Version update to ${newVersion}`);
+      const { enable, threshold = 100 } = __options.updateVersion || {};
+      if (enable) {
+        const pkg = JSON.parse(import_node_fs.default.readFileSync(pkgPath, "utf-8"));
+        oldVersion = pkg.version;
+        newVersion = incrementVersion(pkg.version, threshold);
+        pkg.version = newVersion;
+        import_node_fs.default.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), "utf-8");
+        log(`Version update succeed! ${oldVersion} -> ${newVersion}`);
+      }
     },
-    buildEnd(error) {
+    async buildEnd(error) {
       if (!error) {
         buildSuccessful = true;
+        await Promise.all([
+          gitCommit(__options.gitCommit || { enable: false }),
+          ftpUpload(__options.ftpUpload || { enable: false })
+        ]);
       }
       if (error && !buildSuccessful && oldVersion !== "") {
-        const pkg = JSON.parse(import_node_fs.default.readFileSync(pkgPath, "utf-8"));
-        pkg.version = oldVersion;
-        import_node_fs.default.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), "utf-8");
+        revertVersion(oldVersion);
       }
     }
   };
+}
+function revertVersion(version) {
+  const pkg = JSON.parse(import_node_fs.default.readFileSync(pkgPath, "utf-8"));
+  pkg.version = version;
+  import_node_fs.default.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), "utf-8");
+  log(`Version reverted to ${version}`);
+}
+async function ftpUpload(options) {
+  const { enable, host, user, password, localPath, remotePath, verbose = false, secure } = options;
+  if (!enable) return;
+  if (host && user && password && localPath && remotePath) {
+    if (!import_node_fs.default.existsSync(localPath)) {
+      throw new Error(`Local file not found: ${localPath}`);
+    }
+    const client = new import_basic_ftp.Client();
+    client.ftp.verbose = verbose;
+    try {
+      await client.access({ host, user, password, secure });
+      await client.ensureDir(remotePath);
+      await client.uploadFromDir(localPath);
+      log(`Upload succeed! ${localPath} -> ${remotePath}`);
+    } catch (err) {
+      console.error("Upload failed:", err);
+    } finally {
+      client.close();
+    }
+  } else {
+    throw new Error('Params error. "host", "user", "password", "localPath", and "remotePath" are required.');
+  }
+}
+async function gitCommit(options) {
+  const { enable, push, tag } = options || {};
+  if (!enable) return;
+  try {
+    try {
+      (0, import_node_child_process.execSync)("git add .", { stdio: "inherit" });
+      const commitMessage = `Auto-commit: build on ${(/* @__PURE__ */ new Date()).toISOString()}`;
+      (0, import_node_child_process.execSync)(`git commit -m "${commitMessage}" --no-verify`, { stdio: "inherit" });
+      if (tag) {
+        (0, import_node_child_process.execSync)(`git tag -a v${newVersion} -m "Auto-tag version v${newVersion}"`, { stdio: "inherit" });
+      }
+      if (push) {
+        (0, import_node_child_process.execSync)("git push", { stdio: "inherit" });
+        if (tag) {
+          (0, import_node_child_process.execSync)(`git push origin v${newVersion}`, { stdio: "inherit" });
+        }
+      }
+      log(`Git commit successfully`);
+    } catch (error) {
+      console.error("Git commit failed", error);
+    }
+  } catch (error) {
+    console.error("Git commit failed", error);
+  }
 }
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,29 @@
 import { Plugin } from 'vite';
 
-interface AutoVersionPluginOptions {
-    threshold?: number;
-    disabled?: boolean;
+interface ViteAfterBuildPluginOptions {
+    updateVersion?: {
+        enable?: boolean;
+        threshold: number;
+    };
+    gitCommit?: GitCommitOptions;
+    ftpUpload?: FtpUploadOptions;
+}
+interface FtpUploadOptions {
+    enable: boolean;
+    host: string;
+    user: string;
+    password: string;
+    secure: boolean;
+    localPath: string;
+    remotePath: string;
+    verbose?: boolean;
+}
+interface GitCommitOptions {
+    enable?: boolean;
+    push?: boolean;
+    tag?: boolean;
 }
 
-declare function autoVersionPlugin(options?: AutoVersionPluginOptions): Plugin;
+declare function autoVersionPlugin(options?: ViteAfterBuildPluginOptions): Plugin;
 
 export { autoVersionPlugin as default };
